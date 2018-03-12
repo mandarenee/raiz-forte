f9dfdd1f05d77173a4ae4470597fd149658792db
Amanda <mandakom@uw.edu>
2018-03-11 19:48:49 -0700

    updating aws var names

--- config/paperclip.rb
+++ config/paperclip.rb
@@ -4,7 +4,7 @@ unless Rails.env.development?
 		path: '/:class/:attachment/:id_partition/:style/:filename',
 		storage: :s3,
 		s3_credentials: {
-		  bucket: 				ENV['S3_BUCKET_NAME'],
+		  bucket: 				ENV['AWS_S3_BUCKET'],
 		  access_key_id: 		ENV['AWS_ACCESS_KEY_ID'],
 		  secret_access_key: 	ENV['AWS_SECRET_ACCESS_KEY']
 		}
