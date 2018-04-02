c91ad153a18cd669c2af3a428c529c84fc85bf21
Amanda <mandakom@uw.edu>
2018-04-01 22:33:11 -0700

    Allowing past articles to get a slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ApplicationRecord
     default_scope { order(created_at: :asc) }
     extend FriendlyId
     friendly_id :title, use: :slugged
+
+    def should_generate_new_friendly_id?
+        slug.nil? || title_changed?
+    end
 end
